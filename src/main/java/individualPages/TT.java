30e694c6714657c89608da16d3e2f957fcf184e1
Vignesh <Vignesh@TSS-LT-12>
2026-01-14 19:08:34 +0530

    Conflict Files updated in TT

--- src/main/java/individualPages/TT.java
+++ src/main/java/individualPages/TT.java
@@ -3,9 +3,15 @@ package individualPages;
 public class TT {
 	
 	private void method1() {
-		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("Hello");
+		System.out.println("Hell4646o");
+		System.out.println("Hell44o");
+		System.out.println("Hel23232lo");
+		
+		
+		System.out.println("TT");
+		System.out.println("Hell1o");
+		System.out.println("Hel1lo");
+		
 	}
 
 }
