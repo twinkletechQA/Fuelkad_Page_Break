5e8d43a8e789b7610aaa50e07d185d93cee836fb
Vasan <vasan@twinkletech.com>
2026-01-14 18:56:02 +0530

    TT file has been modified

--- src/main/java/individualPages/TT.java
+++ src/main/java/individualPages/TT.java
@@ -3,9 +3,9 @@ package individualPages;
 public class TT {
 	
 	private void method1() {
-		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("Hello");
+		System.out.println("Hel1lo");
+		System.out.println("Hell1o");
+		System.out.println("Hel1lo");
 	}
 
 }
